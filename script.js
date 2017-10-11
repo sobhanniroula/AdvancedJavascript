d6974c26c63cf1a274568a5734a5ee72c8603893
sobhanniroula <sobhan_niroula@yahoo.com>
2017-10-12 00:37:40 +0300

    Inheritance and Prototype

--- script.js
+++ script.js
@@ -11,14 +11,22 @@ var Person = function (name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-    this.calculateAge = function () {
-        console.log(2017 - this.yearOfBirth);
-    }
 };
 
+Person.prototype.calculateAge = function () {
+    console.log(2017 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
 var john = new Person('John', 1990, 'teacher');
-
-john.calculateAge();
-
 var jane = new Person('Jane', 1969, 'designer');
 var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
