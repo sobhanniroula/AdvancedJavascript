2f40fc0cb3a8a58858dbce0e4413bad39cddc22f
sobhanniroula <sobhan_niroula@yahoo.com>
2017-10-11 23:58:36 +0300

    Second commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// function constructor
+
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+var Person = function (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+    this.calculateAge = function () {
+        console.log(2017 - this.yearOfBirth);
+    }
+};
+
+var john = new Person('John', 1990, 'teacher');
+
+john.calculateAge();
+
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
