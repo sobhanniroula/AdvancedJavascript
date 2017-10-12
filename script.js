140fb77bb8a6d5db2d6bd6d81b7dbf63477d7744
sobhanniroula <sobhan_niroula@yahoo.com>
2017-10-12 22:39:48 +0300

    Functions returning functions

--- script.js
+++ script.js
@@ -34,13 +34,13 @@ console.log(mark.lastName);
 
 
 
-
+/*
 
 // Object.create
 
 var personProto = {
     calculateAge: function () {
-        console.lot(2017 - yearOfBirth);
+        console.log(2017 - yearOfBirth);
     }
 };
 
@@ -60,3 +60,82 @@ var jane = Object.create(personProto, {
         value: 'designer'
     }
 });
+
+*/
+
+
+
+/*
+
+// Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2017 - el;
+}
+
+function isFUllAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFUllAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
+
+*/
+
+
+
+
+
+// First class function: functions returning functions
+
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+            console.log(name + ', can you explain what UI design is?');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log('What do you teach, ' + name + '?');
+        }
+    } else {
+        return function (name) {
+            console.log('Hi ' + name + ', what do you do?');
+        }
+    }
+}
+
+var tearcherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+tearcherQuestion('John');
+tearcherQuestion('Jane');
+tearcherQuestion('Ram');
+tearcherQuestion('Shyam');
+designerQuestion('Hari');
+designerQuestion('Sita');
+
+interviewQuestion('teacher')('Sita');
